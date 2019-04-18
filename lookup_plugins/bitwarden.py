48e08099822380dd99f527c41b692406050e7410
Mickaël Perrin <dev@mickaelperrin.fr>
2019-04-19 00:11:34 +0200

    [FEATURE] Plugin with simple test

---
+++ lookup_plugins/bitwarden.py
@@ -0,0 +1,63 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+from bitwarden_simple_cli.CliSimple import CliSimple
+__metaclass__ = type
+
+DOCUMENTATION = """
+      lookup: bitwarden
+        author: Mickaël Perrin <contact@mickaelperrin.fr> 
+        version_added: "1.0"
+        short_description: fast lookup of secrets stored in bitwarden 
+        description:
+            - This plugin uses the bitwarden-simple-cli python module that implements a faster way to retrieve
+            secrets from a bitwarden vault.
+        requirements:
+            - pip module bitwarden-simple-cli 
+            - BW_SESSION environment variable retrieven through the `bw unlock` command. 
+        options:
+          _uuid:
+            description: uuid of the cipher to retrieve
+            required: True
+          field:
+            description: the name of the field to be retrieven 
+            (name, username, password, notes, uri or any custom field)
+            default: password
+            required: False
+"""
+
+EXAMPLES = """
+- name: get 'password' 
+  debug:
+    msg: "{{ lookup('bitwarden', 'fe6e74aa-a099-4cc1-ae8e-aa3000d02c1' }}"
+- name: get 'password' 
+  debug:
+    msg: "{{ lookup('bitwarden', 'fe6e74aa-a099-4cc1-ae8e-aa3000d02c1', field='password' }}"
+- name: get 'username'
+  debug:
+    msg: "{{ lookup('bitwarden', 'fe6e74aa-a099-4cc1-ae8e-aa3000d02c1', field='username' }}"
+- name: get 'custom_field'
+  debug:
+    msg: "{{ lookup('bitwarden', 'fe6e74aa-a099-4cc1-ae8e-aa3000d02c1', field='custom_field' }}"
+"""
+
+try:
+    from __main__ import display
+except ImportError:
+    from ansible.utils.display import Display
+    display = Display()
+
+
+class LookupModule(LookupBase):
+
+    def run(self, uuids, **kwargs):
+        ret = []
+        field = kwargs.get('field', 'password')
+        for uuid in uuids:
+            display.debug("Bitwarden lookup field: %s of uuid: %s" % (field, uuid))
+            try:
+                ret.append(CliSimple('bw-simple', 'get', field, uuid).run().rstrip())
+            except AnsibleParserError:
+                raise AnsibleError("could not locate uuid: %s" % uuid)
+        return ret
---
+++ lookup_plugins/bitwarden_test.py
@@ -0,0 +1,31 @@
+import pytest
+from os import path
+from lookup_plugins.ansible_bitwarden_lookup_plugin import LookupModule
+
+def common_data(item):
+    return dict(
+        BW_SESSION='Tyy0rDgzvA/jgHsqUtKIgNnAWaRtHKZoSs6pa10qWQf0QmFtd2/xn8TNJy8Fu2nPRNVDpn3k7tu49W1pQVU8Zg==',
+        nl='\n',
+        organization_id='1ff51ccd-0a25-46a2-a3cd-aa3000cfa874',
+        protected_key='ArxoewBOPtCYZKqn34f8CoMQVUrNZnrhGU9OYuJu8UpB7DngEwf/TEHjbxPJJhUDG+DnPQR76J9d12/4tnGT5C6ZaLxxInRooT4AuX2ljIrSppCee1AvzIMu7ljGhR++ng==',
+        protected_key_decoded=b'/lCGAMDGAq+mjRP3FJv+VNDDnbrYcVGnsiPeXTm4NfU=',
+        test_database_filename='data.json',
+        user_email='dev+bitwarden@mickaelperrin.fr',
+        user_id='03780246-7f1d-4221-8615-aa3000cd8123',
+        uuid_login_organization='fe6e74aa-a099-4cc1-ae8e-aa3000d02c14',
+        uuid_login_personal='fd8870cc-3659-40aa-9492-aa3000cedbb3',
+        uuid_note_personal='450cbad2-580b-4523-bce8-aa3000cf641a'
+    ).get(item)
+
+
+@pytest.fixture
+def bw_session(monkeypatch):
+    monkeypatch.setenv('BW_SESSION', common_data('BW_SESSION'))
+    monkeypatch.setenv('BITWARDENCLI_APPDATA_DIR', path.dirname(__file__))
+
+
+@pytest.mark.usefixtures("bw_session")
+def test_get_login_organization_password(capsys):
+    LookupModule().run([common_data('uuid_login_organization')], field='password')
+    std = capsys.readouterr()
+    assert std.out == 'acme_login1_password'
